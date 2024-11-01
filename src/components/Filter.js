5116e5da8d1f71bc8cd68356ba74b110b716adff
Kunal Sable <kunalsable9990@gmail.com>
2024-11-01 23:36:54 +0530

    Some minor updates

--- src/components/Filter.js
+++ src/components/Filter.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 function Filter(props){
 
-    const myCourse = (event) => {
+    const myCourseHandler = (event) => {
         // if(props.element != event.target.textContent){
             props.setElement(event.target.textContent);
         // }
@@ -11,10 +11,10 @@ function Filter(props){
 
     return(
         <div className='flex justify-center '>
-            <div className="nav-elements m-4 ">
+            <div className="nav-elements m-4 flex flex-wrap">
                 {
                     props.courseName.map((course) =>{
-                        return <button onClick={myCourse} className={`filter-btn font-semibold ml-4 bg-[#2d2d35] text-white p-2 text-[18px] rounded-lg ${
+                        return <button onClick={myCourseHandler} className={`filter-btn font-semibold ml-4 bg-[#2d2d35] text-white p-2 text-[18px] rounded-lg ${
                             props.element === course.title ? 'border-2 border-white' : 'border-2 border-transparent'
                           }`} key={course.id}>{course.title}</button>
                     })
--- src/components/Spinner.js
+++ src/components/Spinner.js
@@ -4,10 +4,10 @@ function Spinner(){
     return(
         <div>
             <div className="flex justify-center items-center h-full">
-          <div className="loader text-white">
-            <div className='spinner'></div>
-            loading
-            </div> {/* Add your loading animation here */}
+            <div className="loader text-white flex flex-col space-y-7">
+                <div className='spinner'></div>
+                <p className="font-bold">Loading...</p>
+            </div> 
             </div>
         </div>
     )
